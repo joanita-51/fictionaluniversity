8d5fbab755b711a7a888cd655b96356dd5a15744
joanita-51 <nakityojoanita51@gmail.com>
2022-10-06 12:46:30 +0300

    creating a new theme

---
+++ index.php
@@ -0,0 +1,16 @@
+<!--PHP Functions  -->
+<?php 
+ function greet($name, $color){
+   echo "<p>Hello, my name is $name and my favourite color is $color </p>";
+ }
+
+ greet('John','blue');
+ greet('Jane','green');
+
+?>
+<!-- some of the Wordpress prebuilt functions
+   ?php bloginfo(); ?>
+    ?php bloginfo('description'); ?>
+-->
+
+

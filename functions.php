fede8ef8fb51949560dbf825b028d4b1010aaf5f
joanita-51 <nakityojoanita51@gmail.com>
2022-10-08 23:35:23 +0300

    loading css and javascript file into the static site

--- functions.php
+++ functions.php
@@ -8,9 +8,17 @@ function get_stylesheet_uri(){
 function add_action(){
     echo("This function gives wordpress instruction on what to do");
 }
+function get_theme_file_uri(){
+  echo ("This wordpress function will generate the path to our theme folder all on its own");
+}
+function wp_enqueue_script(){
+  echo ("Loading the js file to show the slideshow behaviour");
+}
 // This is a custom function
 function files() {
-  wp_enqueue_style( 'style', get_stylesheet_uri() );
+  wp_enqueue_script( 'university_main_js', get_theme_file_uri('/build/index.js'), array('jquery'),'1.0',true );
+  wp_enqueue_style( 'university_main_styles', get_theme_file_uri('/build/style-index.css') );
+  wp_enqueue_style( 'university_extra_styles', get_theme_file_uri('/build/index.css') );
   wp_enqueue_style( 'font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css' );
   wp_enqueue_style( 'google-custom-fonts', "//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700>"
  );

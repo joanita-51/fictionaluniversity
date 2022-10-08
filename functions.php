ea06ac4368800b66dc1aa22dd83526b43407d520
joanita-51 <nakityojoanita51@gmail.com>
2022-10-08 23:30:46 +0300

    displaying the icons and the desired font

--- functions.php
+++ functions.php
@@ -11,7 +11,11 @@ function add_action(){
 // This is a custom function
 function files() {
   wp_enqueue_style( 'style', get_stylesheet_uri() );
+  wp_enqueue_style( 'font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css' );
+  wp_enqueue_style( 'google-custom-fonts', "//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700>"
+ );
 } 
   add_action( 'wp_enqueue_scripts', 'files' );
+
 
 ?>

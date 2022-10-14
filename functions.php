6eb6d4d0640ef3d65008a809bfe995ba721558a3
joanita-51 <nakityojoanita51@gmail.com>
2022-10-14 08:56:35 +0300

    after_setup_theme event

--- functions.php
+++ functions.php
@@ -14,6 +14,12 @@ function get_theme_file_uri(){
 function wp_enqueue_script(){
   echo ("Loading the js file to show the slideshow behaviour");
 }
+function add_theme_support(){
+  echo 'This function will enable a feature for the theme in this case it enables the title tag theme';
+}
+function register_nav_menu(){
+  echo 'This function registers menu locations in our theme';
+}
 // This is a custom function
 function files() {
   wp_enqueue_script( 'university_main_js', get_theme_file_uri('/build/index.js'), array('jquery'),'1.0',true );
@@ -23,7 +29,14 @@ function files() {
   wp_enqueue_style( 'google-custom-fonts', "//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700>"
  );
 } 
+function university_features(){
+  add_theme_support ('title-tag');
+  register_nav_menu('headerMenuLocation','Header Menu Location');
+  register_nav_menu('footerLocationOne', 'Footer Location One');
+  register_nav_menu('footerLocationTwo','Footer Location Two');
+}
   add_action( 'wp_enqueue_scripts', 'files' );
+  add_action('after_setup_theme','university_features');
 
 
 ?>
